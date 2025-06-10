a79faebd0f7ae206849ff7ac2c956da215b4e7e8
afancyfrog <tanyafreese@gmail.com>
2025-06-10 14:13:56 +0200

    Affection Meters done

--- MateyAssignment3/Assets/Scripts/SettingsManager.cs
+++ MateyAssignment3/Assets/Scripts/SettingsManager.cs
@@ -4,8 +4,14 @@ using UnityEngine.UI;
 
 public class SettingsManager : MonoBehaviour
 {
+    public GameObject pauseMenu;
     public GameObject settingsPanel;
+    public GameObject choiceHelpPanel;
+    public GameObject exitGameWarning;
+
     public Slider volumeSlider;
+    // add the other sliders here if you need owo
+
     public Slider textSpeedSlider;
     public float typeSpeed;
 
@@ -13,6 +19,10 @@ public class SettingsManager : MonoBehaviour
     void Start()
     {
         settingsPanel.SetActive(false);
+        choiceHelpPanel.SetActive(false);
+        pauseMenu.SetActive(false);
+        exitGameWarning.SetActive(false);
+
         if (PlayerPrefs.HasKey("typeSpeed"))
         {
             LoadTextSpeed();
@@ -48,21 +58,41 @@ public class SettingsManager : MonoBehaviour
          textSpeedSlider.value = PlayerPrefs.GetFloat("typeSpeed");
      }
 
+    public void OnPauseButton()
+    {
+        pauseMenu.SetActive(true);
+    }
+
     public void OnSettingsButton()
     {
+        //pauseMenu.SetActive(false);
         settingsPanel.SetActive(true);
 
     }
 
+    public void OnExitButton() 
+    { 
+        exitGameWarning.SetActive(true);
+    }
     
+
+    public void OnChoiceHelpButton()
+    {
+        choiceHelpPanel.SetActive(true);
+    }
+
+    public void OpenGoogleDoc()
+    {
+        Application.OpenURL("https://www.google.com/");
+    }
 
     //Title:VOLUME SLIDER IN UNITY
     //Author: Solo Game Dev
     //Date: 24 April 2024
     //Code version: unknown
     //Availability: https://www.youtube.com/watch?v=prT-PMl4JSA&ab_channel=SoloGameDev
-       
-        public void SetVolume()
+
+    public void SetVolume()
         {
             AudioListener.volume = volumeSlider.value;
             SaveVolume();
