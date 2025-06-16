656e19ce214c6210cf1915872ead84c342d17b62
afancyfrog <tanyafreese@gmail.com>
2025-06-17 01:16:27 +0200

    fixes

--- MateyAssignment3/Assets/Scripts/BlackoutScript.cs
+++ MateyAssignment3/Assets/Scripts/BlackoutScript.cs
@@ -5,10 +5,33 @@ public class BlackoutScript : MonoBehaviour
     public GameObject blackScreen;
     public int clicks;
     private int clicksCounter;
+    private bool canAdvanceVideo = false;
+    private bool previousWaitingForContinue = false;
 
+    public StoryManager storyManager;
     private void Start()
     {
         blackScreen.SetActive(true);
+    }
+
+    void Update()
+    {
+
+        if (!previousWaitingForContinue && storyManager.waitingForContinue)
+        {
+            canAdvanceVideo = true;
+            Debug.Log("Can now advance video");
+        }
+
+        previousWaitingForContinue = storyManager.waitingForContinue;
+
+
+        if (canAdvanceVideo && Input.GetKeyDown(KeyCode.Space))
+        {
+            continueButton();
+            canAdvanceVideo = false;
+            Debug.Log("Advanced video, resetting flag");
+        }
     }
 
     public void skipbutton()
