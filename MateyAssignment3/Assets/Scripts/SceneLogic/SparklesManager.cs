3dc034cda4600503801d78d6b355c413d9e429ca
afancyfrog <tanyafreese@gmail.com>
2025-06-17 14:11:44 +0200

    sparkles done

---
+++ MateyAssignment3/Assets/Scripts/SceneLogic/SparklesManager.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+
+public class SparklesManager : MonoBehaviour
+{
+    public GameObject ParticleObject;
+    public ParticleSystem sparkles;
+    public int clicks;
+    private int clicksCounter;
+    private bool canAdvance = false;
+    private bool previousWaitingForContinue = false;
+
+    public StoryManager storyManager;
+    private void Start()
+    {
+        if(GameStateManager.CurrentState == GameStateManager.gameState.Day2 || GameStateManager.CurrentState == GameStateManager.gameState.Combat)
+        {
+            ParticleObject.SetActive(true);
+        }
+        else
+        {
+            ParticleObject.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+
+        if (!previousWaitingForContinue && storyManager.waitingForContinue)
+        {
+            canAdvance = true;
+        }
+
+        previousWaitingForContinue = storyManager.waitingForContinue;
+
+
+        if (canAdvance&& Input.GetKeyDown(KeyCode.Space))
+        {
+            continueButton();
+            canAdvance = false;
+        }
+    }
+
+    public void skipbutton()
+    {
+        if (clicks > clicksCounter)
+        {
+            sparkles.Play();
+        }
+        else
+        {
+            ParticleObject.SetActive(false);
+        }
+    }
+
+    public void continueButton()
+    {
+        clicksCounter++;
+        if (clicksCounter == clicks)
+        {
+            sparkles.Play();
+        }
+        if (clicks < clicksCounter)
+        {
+            ParticleObject.SetActive(false);
+        }
+    }
+
+}
