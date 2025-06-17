7f79c546538207c550d3a5fa841d4261a140b329
afancyfrog <tanyafreese@gmail.com>
2025-06-17 16:12:06 +0200

    pearl human sparkles

--- MateyAssignment3/Assets/Scripts/SceneLogic/PearlDay4Logic.cs
+++ MateyAssignment3/Assets/Scripts/SceneLogic/PearlDay4Logic.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 
 
@@ -9,6 +10,11 @@ public class PearlDay4Logic : MonoBehaviour
     public Image Pearl;
     public Sprite Mermaid;
 
+    public SparklesManager SparklesManager;
+    public TMP_Text Dialogue;
+    public string Line;
+    private int clicks;
+
     private void Start()
     {
         if (GameStateManager.CurrentState == GameStateManager.gameState.Day2)
@@ -16,6 +22,9 @@ public class PearlDay4Logic : MonoBehaviour
             blackout.SetActive(true);
             Pearl.sprite = Mermaid;
             Pearl.SetNativeSize();
+            clicks = 0;
+            Line = "This Kraken thing...it's not like the sea beasts I'm used to. It's ancient, primal and I feel like it knows we're coming.";
+
         }
         else
         {
@@ -23,4 +32,17 @@ public class PearlDay4Logic : MonoBehaviour
         }
     }
 
+    private void Update()
+    {
+        if ((GameStateManager.CurrentState == GameStateManager.gameState.Day2) && (clicks == 0)) 
+        {
+           if(Dialogue.text.Contains(Line))
+            {
+                SparklesManager.ParticleObject.SetActive(true);
+                SparklesManager.sparkles.Play();
+                clicks++;
+            }
+
+        }
+    }
 }
